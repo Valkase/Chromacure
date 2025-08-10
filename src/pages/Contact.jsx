68422107ad6f4e18f97fcce59765b0d61f6c0726
Ahmed Mohsen <ahmed.mohsen2305@gmail.com>
2025-08-10 22:25:53 +0300

    Brute Force solution for the firebase bug

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,21 +10,15 @@ import { getAuth, signInWithCustomToken, signInAnonymously } from 'firebase/auth
 // __firebase_config first, then falls back to environment variables. This prevents the
 // "process is not defined" error in a local development environment.
 // I hate Firebase man
-const firebaseConfig = (() => {
-  if (typeof __firebase_config !== 'undefined') {
-    return JSON.parse(__firebase_config);
-  } else {
-    return {
-      apiKey: process.env.REACT_APP_API_KEY,
-      authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-      projectId: process.env.REACT_APP_PROJECT_ID,
-      storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-      messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-      appId: process.env.REACT_APP_APP_ID,
-      measurementId: process.env.REACT_APP_MEASUREMENT_ID
-    };
-  }
-})();
+const firebaseConfig = {
+  apiKey: "AIzaSyBmMMzrSolqcqy0W-BZ5nSUZTrcxjNxSX8",
+  authDomain: "chromacure-4aac2.firebaseapp.com",
+  projectId: "chromacure-4aac2",
+  storageBucket: "chromacure-4aac2.firebasestorage.app",
+  messagingSenderId: "659675138326",
+  appId: "1:659675138326:web:70317441370a6d682ee837",
+  measurementId: "G-LK4JQB238F"
+};
 
 // Added a console.log here to print the API key
 console.log("Firebase API Key:", firebaseConfig.apiKey);
